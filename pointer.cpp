a6a16f2df5575bea7dbdafaa58b6694f466803fe
Rasel_ <amrasel.049@gmail.com>
2025-11-09 15:03:13 +0600

    edit done

--- pointer.cpp
+++ pointer.cpp
@@ -1,11 +1,17 @@
+ // data_type *pointer variable name = integer type container
+ //for integer: int *ptri = &x; x is an integer variable
+ //for floating: float *ptrf = &x; x is a float variable
+
+
 #include<iostream>
 using namespace std;
 int main(){
     int x=10;
-    int *ptr = &x;
-    cout<<&x<<"\n";
-    cout<< ptr<<"\n"; 
-    cout<<*ptr<<"\n";
+    int *ptr = &x;     // pointer is one kind of variable, which can store address. 
+    cout<<&x<<"\n";  //prints the address
+    cout<< ptr<<"\n"; // prints the address
+    cout<<*ptr<<"\n"; // prints the value of the variable that the pointer is pointing
+                       
 
     *ptr=20;
     cout<<ptr<<"\n";
